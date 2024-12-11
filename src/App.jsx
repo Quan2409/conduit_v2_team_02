ebe7529d5afccca92ec459773c9da90be247f772
HaiiLongg <longlhgdh190623@fpt.edu.vn>
2024-12-11 14:15:01 +0700

    ui signin & signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import Signin from "./page/Signin/Signin"; // Import Signin component
+import Signup from "./page/Signin/Signup"; // Import Signup component
+import { BrowserRouter, Routes, Route, Link, Navigate} from "react-router-dom";
 
 const App = () => {
+  const [isLogin, setIsLogin] = useState(false); // Trạng thái theo dõi người dùng đã đăng nhập
+  const [isSignin, setIsSignin] = useState(true); // Trạng thái chuyển đổi giữa Signin và Signup
+
   return (
-    <div>
-      <h1>Hello Conduit Version 2</h1>
+    <div className="App">
+      <BrowserRouter>
+      <Routes> 
+      <Route path="/signin" element={<Signin/>}/>
+      <Route path="/signup" element={<Signup/>}/>
+      </Routes>
+      </BrowserRouter>
+       
     </div>
   );
 };
